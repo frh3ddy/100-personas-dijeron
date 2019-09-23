b1447d66a133e4d7591ab500d6b9861eda92686e
fredy <fr3ddy@me.com>
2019-09-23 09:09:55 -0500

    add samsara css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ import RemarkContext, {Panel} from 'remarkui'
 function App() {
   return (
     <RemarkContext>
-            <Panel alignment='center' width={300} height={300} color='red'/>
+            <Panel alignment='center' width={300} height={300} color='red'>
+              <Panel alignment='bottomRight' width={100} height={100} color='tomato'>
+                <Panel width={25} height={25} color='blue'/>
+              </Panel>
+            </Panel>
+            <Panel width={300} height={300} color='red'>
+              <Panel alignment='bottomRight' width={100} height={100} color='tomato'>
+                <Panel width={25} height={25} color='blue'/>
+              </Panel>
+            </Panel>
+            <Panel alignment='bottomRight' width={300} height={300} color='red'>
+              <Panel alignment='bottomRight' width={100} height={100} color='tomato'>
+                <Panel width={25} height={25} color='blue'/>
+              </Panel>
+            </Panel>
     </RemarkContext>
   );
 }
